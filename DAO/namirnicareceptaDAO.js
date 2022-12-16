afa8d5b7121269eaf7f58e70100ff49ef0965a2a
Filip Šoštarić <fsostaric20@foi.hr>
2022-12-16 14:23:39 +0100

    Dodani zatvori vezu na upite za namirnicereceptaDAO

--- DAO/namirnicareceptaDAO.js
+++ DAO/namirnicareceptaDAO.js
@@ -26,12 +26,14 @@ class namirnicareceptaDAO{
         let sql = "INSERT INTO heroku_d1561a1a0615483.namirnica_recepta (recept_id,namirnica_id,kolicina) VALUES (?,?,?)";
         let podaci = [Rid,Nid,kolicina];
         await this.baza.izvrsiUpit(sql,podaci);
+        this.baza.zatvoriVezu();
         return true;
     }
     
     obrisi = async function (id) {
         let sql = "DELETE FROM heroku_d1561a1a0615483.namirnica_recepta WHERE recept_id=?";
         await this.baza.izvrsiUpit(sql,[id]);
+        this.baza.zatvoriVezu();
         return true;
     }    
 
